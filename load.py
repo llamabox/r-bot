51e1d73b3c4f6003bc8a66897f8ebba88c187c7e
Davide <dcarboni@gmail.com>
2024-01-11 17:13:42 +0100

    tests with pdf done

--- load.py
+++ load.py
@@ -4,27 +4,44 @@ from llama_index import (
     VectorStoreIndex,
     ServiceContext,
     download_loader,
+    SimpleDirectoryReader
 )
 from llama_index.llms import Ollama
 from llama_index.storage.storage_context import StorageContext
 from llama_index.vector_stores.qdrant import QdrantVectorStore
 
+# Assign the name for the collection, it must be a directory with the same
+# name containing the files to be loaded
+# kbase = 'kbase-paulgraham'
+kbase = 'kbase-rmassimo'
+loader = SimpleDirectoryReader(kbase)
 
-JSONReader = download_loader("JSONReader")
-loader = JSONReader()
-documents = loader.load_data(Path('tinytweets.json'))
+
+
+
+
+
+# Modify with the proper path
+#path = Path('tinytweets.json')
+documents = loader.load_data()
 
 client = qdrant_client.QdrantClient(
     path="./qdrant_data"
 )
-vector_store = QdrantVectorStore(client=client, collection_name="tweets")
+
+vector_store = QdrantVectorStore(client=client, collection_name=kbase)
 storage_context = StorageContext.from_defaults(vector_store=vector_store)
 
+
+# Select your model, eg. mistral
 llm = Ollama(model="mistral")
 service_context = ServiceContext.from_defaults(llm=llm,embed_model="local")
 
 index = VectorStoreIndex.from_documents(documents,service_context=service_context,storage_context=storage_context)
 
 query_engine = index.as_query_engine()
-response = query_engine.query("What does the author think about Star Trek? Give details.")
+
+# Prepare your smoke test question
+# "What is the name of the author's dog"
+response = query_engine.query("What does the author think about Kids? Give details.")
 print(response)
--- verify.py
+++ verify.py
@@ -9,12 +9,12 @@ from llama_index.vector_stores.qdrant import QdrantVectorStore
 client = qdrant_client.QdrantClient(
     path="./qdrant_data"
 )
-vector_store = QdrantVectorStore(client=client, collection_name="tweets")
+vector_store = QdrantVectorStore(client=client, collection_name="kbase-rmassimo")
 
 llm = Ollama(model="mistral")
 service_context = ServiceContext.from_defaults(llm=llm,embed_model="local")
 
 index = VectorStoreIndex.from_vector_store(vector_store=vector_store,service_context=service_context)
 query_engine = index.as_query_engine(similarity_top_k=20)
-response = query_engine.query("Does the author like SQL? Give details.")
+response = query_engine.query("Does the author like startups? Give details.")
 print(response)
