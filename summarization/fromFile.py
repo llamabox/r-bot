2f712270212537add31327fdffbf375f8e4d375d
davide <davide@192.168.1.53>
2024-03-22 14:26:31 +0100

    new notebook and cnn dataset

--- summarization/fromFile.py
+++ summarization/fromFile.py
@@ -1,7 +1,7 @@
 print("loading ollama...")
 from llama_index.llms import Ollama
 from llama_index import ServiceContext
-llm = Ollama(model="mistral", request_timeout=10.0)
+llm = Ollama(model="mistral", request_timeout=20.0)
 service_context = ServiceContext.from_defaults(llm=llm,embed_model="local")
 print("DONE")
 
@@ -10,15 +10,21 @@ print("DONE")
 print("loading data ...")
 from llama_index import SimpleDirectoryReader
 reader = SimpleDirectoryReader(
-    input_files=["../docs/essay.txt"]
+    input_files=["../docs/dichiarazione-voto.txt"]
 )
 docs = reader.load_data()
 text = docs[0].text
-print("DONE")
+print(text)
 
 
 print("summarizing ...")
 from llama_index.response_synthesizers import TreeSummarize
 summarizer = TreeSummarize(service_context=service_context,verbose=True)
-response =  summarizer.get_response("what is all about?", [text])
+prompt="""
+this is a parlamentary assembly where the speaker named Lombardo, asks to speak and then express his opinions on a subject.  
+Please summarize the speech of the speaker and then answer whether he 
+vote in favour or against the proposed motion?
+Produce answer in Italian.
+"""
+response =  summarizer.get_response(prompt, [text])
 print(response)
